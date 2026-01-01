08a59c4bce6b3e5471c1d2f24b3e9048e592b314
QurioChess <qurio.chess@protonmail.com>
2026-01-01 21:03:32 +0100

    WIP: bench from cli

--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+#include "bench.h"
 #include "uci.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     printf("Qurio - Chess engine\n");
-    main_loop();
 
+    if (argc > 1 && strcmp(argv[1], "bench") == 0) {
+        bench();
+        return 0;
+    }
+
+    main_loop();
     return 0;
 }
